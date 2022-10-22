22814cca2ef977db9b2b8e50f2e23df39eb326c1
Halyson <92060880+HalysonSA@users.noreply.github.com>
2022-10-22 14:08:18 -0300

    new footer design

--- components/Layout/Footer/footer.tsx
+++ components/Layout/Footer/footer.tsx
@@ -8,6 +8,9 @@ import {
     Button,
     Image,
     Flex,
+    Divider,
+    HStack,
+    Center,
 } from '@chakra-ui/react';
 import React, { ReactElement } from 'react';
 
@@ -15,6 +18,8 @@ import { FaInstagram, FaTwitter, FaYoutube, FaFacebookF } from 'react-icons/fa';
 import { IoIosCard } from 'react-icons/io';
 import { MdVerified } from 'react-icons/md';
 import { TbTruckDelivery } from 'react-icons/tb';
+import { BsWhatsapp } from 'react-icons/bs';
+import { MdOutlineMailOutline } from 'react-icons/md';
 
 interface SocialProps {
     children?: ReactElement;
@@ -31,7 +36,7 @@ interface FooterInfoProps {
 const SocialButton = ({ children, label, href }: SocialProps) => {
     return (
         <Button
-            bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+            bg={useColorModeValue('white', 'gray.100')}
             rounded={'full'}
             w={12}
             h={12}
@@ -100,94 +105,105 @@ const InfoFooter = ({ icon, title, description }: FooterInfoProps) => {
 
 export default function Footer() {
     return (
-        <Container
-            maxW="1366px"
+        <Box
+            w="100%"
             bottom={'0'}
             bg="white"
+            borderTopWidth={'1px'}
             color="black"
         >
-            <Flex
-                minH="200px"
-                direction={'row'}
-                wrap={'wrap'}
-                borderBottom={'1px'}
-                borderColor={'gray.200'}
-            >
-                <InfoFooter
-                    icon={<IoIosCard size={'70px'} />}
-                    title={'Pagamentos'}
-                    description={'Cartão de Crédito/Debito, Boleto e PIX'}
-                />
-                <InfoFooter
-                    icon={<MdVerified size={'70px'} />}
-                    title={'Garantia de Satisfação'}
-                    description={'Garantia do Fabricante'}
-                />
-                <InfoFooter
-                    icon={<TbTruckDelivery size={'70px'} />}
-                    title={'Frete Grátis'}
-                    description={'Entrega em Todo o Brasil'}
-                />
-            </Flex>
-            <Container
-                as={Stack}
-                py={4}
-                spacing={4}
-                justify={'center'}
-                align={'center'}
-            >
-                <Image
-                    w="80px"
-                    h="80px"
-                    src="/logo.svg"
-                    alt="Logo"
-                />
-                <Stack
+            <Container maxW={'1366px'}>
+                <Flex
+                    minH="200px"
                     direction={'row'}
-                    spacing={6}
+                    wrap={'wrap'}
+                    borderColor={'gray.200'}
                 >
-                    <Text
-                        onClick={() =>
-                            window.scrollTo({
-                                top: 0,
-                                behavior: 'smooth',
-                            })
-                        }
-                        _hover={{
-                            transform: 'scale(1.2)',
-                            color: 'blackAlpha.800',
-                        }}
-                        cursor={'pointer'}
-                    >
-                        Inicio
-                    </Text>
-                    <LinkFooter
-                        label="Sobre"
-                        href="/about"
+                    <InfoFooter
+                        icon={<IoIosCard size={'70px'} />}
+                        title={'Pagamentos'}
+                        description={'Cartão de Crédito/Debito, Boleto e PIX'}
                     />
-                    <LinkFooter
-                        label="Contato"
-                        href="/contact"
+                    <InfoFooter
+                        icon={<MdVerified size={'70px'} />}
+                        title={'Garantia de Satisfação'}
+                        description={'Garantia do Fabricante'}
                     />
-                </Stack>
+                    <InfoFooter
+                        icon={<TbTruckDelivery size={'70px'} />}
+                        title={'Frete Grátis'}
+                        description={'Entrega em Todo o Brasil'}
+                    />
+                </Flex>
             </Container>
-            <Box
-                borderStyle={'solid'}
-                borderTopWidth={'1px'}
-                borderColor={useColorModeValue('gray.200', 'gray.700')}
+            <Divider />
+
+            <Flex
+                minH="220px"
+                py="5"
+                gap={'20%'}
+                px="10"
+                wrap={'wrap'}
+                direction={['column', 'column', 'column', 'row', 'row']}
+                bg="purple.500"
             >
-                <Container
-                    as={Stack}
-                    py={4}
-                    direction={{ base: 'column', md: 'row' }}
-                    spacing={4}
-                    justify={{ base: 'center', md: 'space-between' }}
-                    align={{ base: 'center', md: 'center' }}
-                >
-                    <Text>©2022 HALLCORP.</Text>
+                <Box mt="2">
+                    <Stack>
+                        <Text
+                            color="white"
+                            fontWeight={'bold'}
+                            fontSize={'xl'}
+                        >
+                            Institucional
+                        </Text>
+                        <Stack color={'whiteAlpha.800'}>
+                            <Text>Política de Privacidade</Text>
+                            <Text>Perguntas Frequentes</Text>
+                        </Stack>
+                    </Stack>
+                </Box>
+                <Stack mt="2">
+                    <Text
+                        color="white"
+                        fontWeight={'bold'}
+                        fontSize={'xl'}
+                    >
+                        Minha conta
+                    </Text>
+                    <Text color={'whiteAlpha.800'}>Meus pedidos</Text>
+                </Stack>
+
+                <Stack mt="2">
+                    <Text
+                        color="white"
+                        fontWeight={'bold'}
+                        fontSize={'xl'}
+                    >
+                        Atendimento
+                    </Text>
+                    <Stack color="whiteAlpha.800">
+                        <HStack>
+                            <BsWhatsapp size={20} />
+                            <Text>(99) 9 9999-9099</Text>
+                        </HStack>
+                        <HStack>
+                            <MdOutlineMailOutline size={20} />
+                            <Text>teste@gmail.com</Text>
+                        </HStack>
+                    </Stack>
+                </Stack>
+
+                <Stack mt="2">
+                    <Text
+                        color="white"
+                        fontWeight={'bold'}
+                        fontSize={'xl'}
+                    >
+                        Redes sociais
+                    </Text>
                     <Stack
                         direction={'row'}
-                        spacing={6}
+                        spacing={3}
                     >
                         <SocialButton
                             label={'Instagram'}
@@ -203,8 +219,78 @@ export default function Footer() {
                             <FaFacebookF size={20} />
                         </SocialButton>
                     </Stack>
-                </Container>
+                </Stack>
+            </Flex>
+            <Stack
+                minH="200px"
+                w="100%"
+                bg="purple.500"
+                px="10"
+                py="5"
+                borderTopWidth={'1px'}
+            >
+                <Text
+                    color="white"
+                    fontWeight={'bold'}
+                    fontSize={'2xl'}
+                >
+                    Formas de Pagamento
+                </Text>
+                <Flex
+                    direction={'row'}
+                    gap="2"
+                    wrap={'wrap'}
+                >
+                    <Image
+                        h="30px"
+                        w="60px"
+                        src="/visa.png"
+                        alt="Visa"
+                    />
+                    <Image
+                        h="30px"
+                        w="60px"
+                        src="/mastercard.png"
+                        alt="Mastercard"
+                    />
+                    <Image
+                        bg="white"
+                        h="30px"
+                        w="60px"
+                        src="/pix.png"
+                        alt="Pix"
+                    />
+                    <Image
+                        h="30px"
+                        w="60px"
+                        src="/boleto.png"
+                        alt="Boleto"
+                    />
+                    <Image
+                        h="30px"
+                        w="60px"
+                        src="/mercadopago.png"
+                        alt="Mercado Pago"
+                    />
+                </Flex>
+            </Stack>
+
+            <Box
+                bg="purple.900"
+                color="white"
+                borderStyle={'solid'}
+                borderColor={useColorModeValue('gray.200', 'gray.700')}
+            >
+                <Box
+                    as={Stack}
+                    py={4}
+                    pl={4}
+                >
+                    <Text fontSize={'sm'}>
+                        ©2022 HALLCORP. Todos os direitos reservados.
+                    </Text>
+                </Box>
             </Box>
-        </Container>
+        </Box>
     );
 }
