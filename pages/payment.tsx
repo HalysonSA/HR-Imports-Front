cee2233bc36fc9e5cb8c78a109ce6f2c8f6e3b3a
Halyson <92060880+HalysonSA@users.noreply.github.com>
2022-12-12 19:52:14 -0300

    disabling login validation

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -2,30 +2,29 @@ import { Box, Center, Flex } from '@chakra-ui/react';
 import StatusBar from '../components/Cart/cartcomponents/statusBar';
 import StripeComponent from '../components/Cart/stripe';
 import Layout from '../components/Layout/Layout';
-import RouterGuard from '../components/RoutingGuard';
 
 const Payment = () => {
     return (
         <Layout>
-            <RouterGuard>
-                <Center m={5}>
-                    <StatusBar />
-                </Center>
-                <Flex
-                    gap={5}
-                    direction={'column'}
-                >
-                    <Flex borderRadius={'20px'}>
-                        <Box
-                            w="100%"
-                            borderRadius={'15px'}
-                            minH="800px"
-                        >
-                            <StripeComponent />
-                        </Box>
-                    </Flex>
+            {/*<RouterGuard>*/}
+            <Center m={5}>
+                <StatusBar />
+            </Center>
+            <Flex
+                gap={5}
+                direction={'column'}
+            >
+                <Flex borderRadius={'20px'}>
+                    <Box
+                        w="100%"
+                        borderRadius={'15px'}
+                        minH="800px"
+                    >
+                        <StripeComponent />
+                    </Box>
                 </Flex>
-            </RouterGuard>
+            </Flex>
+            {/*</RouterGuard>*/}
         </Layout>
     );
 };
